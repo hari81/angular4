e1c84250ff2e7c77f20c8c2d7c29b5f9cea8241e
Karthik Hari <hk.sudda@gmail.com>
2018-09-22 16:48:42 -0400

    adding interface to app

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,17 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   name: string; // = 'John Doe';
   age: number;
-  address: {
-    houseNo: number,
-    street: string,
-    state: string
-  };
+  address: Address;
   hobbies: string[];
+  email: string;
   constructor() { }
 
   ngOnInit() {
     this.name = 'Karthik Suddapalli';
     this.age = 35;
+    this.email = 'karsud81@gmail.com';
     this.address = {
       houseNo: 81,
       street: 'James cook dr',
@@ -26,5 +24,14 @@ export class UserComponent implements OnInit {
     };
     this.hobbies = ['watch youtube', 'codeing', 'careing'];
   }
+  onClick() {
+    console.log('clicked me');
+    this.name = 'Kar suddapalli';
+  }
+}
 
+interface Address {
+  houseNo: number;
+  street: string;
+  state: string;
 }
